5ede3c4cf491f3bc3e66fab5df1d577c6e170bd1
Rico <richardthomaswhitfield@gmail.com>
2017-03-31 15:28:35 -0500

    styling components

--- Peckish/app/containers/Home.js
+++ Peckish/app/containers/Home.js
@@ -15,20 +15,54 @@ class Home extends Component {
 	searchPressed() {
 		this.props.fetchRecipes('bacon,cheese');
 	}
+
+	recipes() {
+		return Object.keys(this.props.searchedRecipes).map( key => this.props.searchedRecipes[key])
+	}
+
 	render() {
-		return <View style={{marginTop: 30}}>
-			<View>
-				<TouchableHighlight onPress={ () => this.searchPressed() }>
+		return <View style={styles.scene}>
+			<View style={styles.searchSection}>
+				<TouchableHighlight style={styles.searchButton} onPress={ () => this.searchPressed() }>
 				<Text>Fetch Recipes</Text>
 				</TouchableHighlight>
 			</View>
-			<ScrollView>
-				
+			<ScrollView style={styles.scrollSection}>
+				{this.recipes().map((recipe) => {
+					return <View key={recipe.href}>
+						<Image source={ { uri: recipe.thumbnail } } style={styles.resultImage} />
+						<Text style={styles.resultText}>{recipe.title}</Text>
+					</View>
+				})}
 			</ScrollView>
 		</View>
 	}
 
 }
+
+const styles = StyleSheet.create({
+	scene: {
+		flex: 1,
+		marginTop: 20,
+	},
+	searchSection: {
+		height: 30,
+		borderBottomColor: '#000',
+		borderBottomWidth: 1,
+		padding: 5,
+	},
+	scrollSection: {
+		flex: 0.8
+	},
+	resultImage: {
+		height: 150,
+	},
+	resultText: {
+		backgroundColor: '#000',
+		color: '#FFF',
+		height: 20,
+	}
+});
 
 function mapStateToProps(state) {
 	return {
